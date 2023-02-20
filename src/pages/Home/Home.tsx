da786a34fcfea63c21c98d4df8ce848ff091af52
Wanted4235 <wanted_09@bk.ru>
2023-02-20 21:18:15 +0300

    add scss module and create 2 pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './Home.module.scss'
+
+type Props = {}
+
+export const Home = (props: Props) => {
+    return (
+        <div className='{s.home}'>Home</div>
+    )
+}
